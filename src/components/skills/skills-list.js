1842ed050c2ff3ba708a98a90b92c1a8268e0e16
sanjarproductions <sanjarproduction4@gmail.com>
2024-01-20 15:07:08 +0500

    first commit

---
+++ src/components/skills/skills-list.js
@@ -0,0 +1,39 @@
+import webdev from "../../assets/images/webdev.jpg";
+import mobiledev from "../../assets/images/mobiledev.jpg";
+import smm from "../../assets/images/smm.webp";
+import uiUx from "../../assets/images/ui-ux.jpg";
+
+export const skillsList = [
+  {
+    title: "Web development",
+    text: "Build, grow your website with our highly skilled team of web developers",
+    hoverImg: webdev,
+    color: "#36A112",
+    x: 0,
+    y: 0,
+  },
+  {
+    title: "Mobile development",
+    text: "Need help creating, improving or transitioning your mobile experiences? Our mobile dev team can help",
+    hoverImg: mobiledev,
+    color: "#E2B671",
+    x: -750,
+    y: 0,
+  },
+  {
+    title: "SMM",
+    text: "Take your social media presence to the next level with our team",
+    hoverImg: smm,
+    color: "#E2B671",
+    x: 0,
+    y: 0,
+  },
+  {
+    title: "UX/UI development",
+    text: "Take your website's desing to the next level, it's time for a new design",
+    hoverImg: uiUx,
+    color: "#36A112",
+    x: -750,
+    y: 0,
+  },
+];
